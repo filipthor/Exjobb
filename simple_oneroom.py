90755678cfa41de97a8eb26ee67272dbcabe611c
filipthor <filipthor@Filips-MacBook-Pro-2.local>
2017-04-04 17:19:09 +0200

    2017-04-04

--- simple_oneroom.py
+++ simple_oneroom.py
@@ -1,6 +1,6 @@
 import os
 import timeit
-from  pylab import *
+from pylab import *
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import sparse
@@ -9,55 +9,83 @@ import scipy.sparse.linalg
 
 class simple_oneroom:
 
-    # initial conditions
-    highTemp = 50
-    lowTemp = 5
-    n = 50
-    N = n - 2
-    dx = 1 / (n + 1)
-    u = np.zeros((n,n))
+    def __init__(self, n=40, solution=[]):
+        self.n = n
+        self.solution = []
 
-    sup = np.ones(N**2 - 1)
+    def main(self):
+        # initial conditions
+        ht = 30
+        lt = 15
 
-    for i in range(1, len(sup)+1):
-        rem = i % N
-        if (rem == 0): sup[i - 1] = 0
-    A = np.diag(ones(N**2 - N), -N) \
-            + np.diag(sup, -1) \
-            + np.diag(-4.*ones(N**2), 0) \
-            + np.diag(sup, 1) \
-            + np.diag(ones(N**2 - N),N)
-    D = sparse.csr_matrix(1 / (dx ** 2) * A)
+        n = self.n
+        N = n - 2
+        Ny = 2*n - 2
+        Nx= N
+        dx = 1 / (N + 1)
 
-    # boundary conditions
-    bc = np.zeros((N, N))
-    bc[0, :] = lowTemp / (dx ** 2)       # y = 0 (v)
-    bc[-1, :] = lowTemp / (dx ** 2)      # y = N (h)
-    bc[:, -1] = lowTemp / (dx ** 2)      # x = N (t)
-    bc[:, 0] = highTemp / (dx ** 2)      # x = 0 (b)
+        # wall temperatures
+        un = ht
+        uw = ht
+        us = lt
+        ue = lt
 
-    bc[0, 0] = bc[0, 0] + highTemp / (dx ** 2)      # bv
-    bc[0, -1] = bc[0, -1] + lowTemp / (dx ** 2)     # bh
-    bc[-1, 0] = bc[-1, 0] + highTemp / (dx ** 2)    # tv
-    bc[-1, -1] = bc[-1, -1] + lowTemp / (dx ** 2)   # th
+        u = np.zeros((n,2*n))
 
-    bc = np.asarray(bc).reshape(-1)  # bc as vector
+        sup = np.ones(Nx*Ny - 1)
 
-    # solve
-    solvetime = timeit.default_timer()
-    utemp = scipy.sparse.linalg.spsolve(D, -bc)
-    solvetime = timeit.default_timer() - solvetime
-    print("Solver took {} seconds to run".format(solvetime))
+        # creating A matrix
+        for i in range(1, len(sup)+1):
+            rem = i % Ny
+            if (rem == 0): sup[i - 1] = 0
+        A = np.diag(ones(Nx*Ny - Ny), -Ny) \
+                + np.diag(sup, -1) \
+                + np.diag(-4.*ones(Nx*Ny), 0) \
+                + np.diag(sup, 1) \
+                + np.diag(ones(Nx*Ny - Ny),Ny)
 
-    # reshape for plot
-    umatrix = np.reshape(utemp, (N, N))
-    u[1:-1, 1:-1] = umatrix
-    u[0, :] = lowTemp  # y = 0
-    u[-1, :] = lowTemp  # y = N
-    u[:, 0] = highTemp  # x = 0
-    u[:, -1] = lowTemp  # x = N
+        D = sparse.csr_matrix((1 /dx ** 2) * A)
 
-    #plt.pcolor(np.flipud(u))
-    plt.pcolor(u)
-    plt.show()
+        # boundary conditions
+        bc = np.zeros((Nx, Ny))
+        bc[:, -1] = ue
+        bc[0, :] = us
+        bc[:, 0] = uw
+        bc[-1, :] = un
+        bc = bc / (dx ** 2)
 
+        bc[0, 0] = bc[0, 0] + lt / (dx ** 2)      # bv
+        bc[0, -1] = bc[0, -1] + lt / (dx ** 2)     # bh
+        bc[-1, 0] = bc[-1, 0] + ht / (dx ** 2)    # tv
+        bc[-1, -1] = bc[-1, -1] + lt / (dx ** 2)   # th
+
+        bc = np.asarray(bc).reshape(-1)  # bc as vector
+
+        # solve
+        solvetime = timeit.default_timer()
+        u_sol_inner = np.reshape(scipy.sparse.linalg.spsolve(D, -bc), (Nx, Ny))
+        solvetime = timeit.default_timer() - solvetime
+        print("Solver took {} seconds to run".format(solvetime))
+
+        # reshape for plot
+        u[1:-1, 1:-1] = u_sol_inner
+        u[0, :] = us
+        u[:, 0] = uw
+        u[:, -1] = ue
+        u[-1, :] = un
+        self.set_solution(u)
+
+    def set_solution(self,solution):
+        self.solution = solution
+
+    def get_solution(self):
+        return self.solution
+
+    def visualize(self):
+        plt.pcolor(self.solution)
+        plt.colorbar()
+        plt.show()
+
+
+
+if __name__ == "__main__": simple_oneroom.main()
